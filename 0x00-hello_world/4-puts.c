8153d1e183690d43a55091c74ff3555ae6751ff4
austindikachi <augustineogazie@gmail.com>
2022-09-08 20:03:58 -0700

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Printing in c
- * 
- * Return: Always 0 (Successful)
- */
+*main-Printing in c 
+*Return: 0
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-        return(0) ;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }	
