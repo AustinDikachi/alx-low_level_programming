5eab0606982d7ec4de052fb86a3f0abc4e543101
austindikachi <augustineogazie@gmail.com>
2022-09-11 19:46:04 -0700

    View c task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - print entry
+*
+*Description : using the main function
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+for (char lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+ putchar(lowercase);
+}
+{
+ putchar('\n');
+}
+return (0);
+}
