4e5d5e72298206c66ab9aa9fa14de26e893321e7
austindikachi <augustineogazie@gmail.com>
2022-09-11 21:49:10 -0700

    Viwe ALX C task 6 code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - print entry
+*
+*Description : using the main function
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+int i;
+for (i = '0' ; i <= '9'; i++)
+{
+putchar(i);
+}
+{
+putchar('\n');
+}
+return (0);
+}
