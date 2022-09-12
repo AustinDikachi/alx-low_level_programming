3016f944913c4a2dee80d02e9bd7a6da661c9ea1
austindikachi <augustineogazie@gmail.com>
2022-09-11 21:57:04 -0700

    View ALX C code for task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - print entry
+*Description : using the main function
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+char lw;
+for (lw = 'z' ; lw >= 'a'; lw--)
+{
+putchar(lw);
+}
+{
+putchar('\n');
+}
+return (0);
+}
