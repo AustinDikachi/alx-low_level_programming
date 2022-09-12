c5a9602bd68e0b45524f99afdf028eb8147e458c
austindikachi <augustineogazie@gmail.com>
2022-09-11 21:38:30 -0700

    View C code for ALX task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - print entry
+*Description : using the main function
+*Return: 0 (success)
+*/
+
+int main(void)
+{
+int i;
+for (i = '0' ; i <= '9';)
+{
+printf("%c", i++);
+}
+{
+printf("\n");
+}
+return (0);
+}
