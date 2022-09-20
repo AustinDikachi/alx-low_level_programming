c597cf76bbb7d2817bd8b557631052a3ef5050ca
austindikachi <augustineogazie@gmail.com>
2022-09-19 20:19:07 -0700

    View pointers arrays and strings CTask 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+*reset_to_98 - reset the value of a given integer to 98
+*Return : nothing
+**/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Contains codes that solves alx 0x05-pointers_arrays_strings tasks
+
