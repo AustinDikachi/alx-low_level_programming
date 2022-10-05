beb05ceb8c53e469618e30e41c3e539cba6ea74a
austindikachi <augustineogazie@gmail.com>
2022-10-05 02:41:49 -0700

    View alx argc_argv command for task o

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - prints the name of the programme
+ *@argc: counts argument
+ *@argv: arguments
+ *Return: always 0 (success
+ */
+int main(int argc, char *argv[])
+{
+
+
+	printf("%s", argv[0]);
+	{
+		putchar('\n');
+	}
+	return (0);
+}
