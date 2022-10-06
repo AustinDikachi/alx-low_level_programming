613e5077792f047e050c8abdfb8a1de350daa34a
austindikachi <augustineogazie@gmail.com>
2022-10-05 17:34:29 -0700

    View alx argc_argv C task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *main - prints all argument it receives
+ *main - prints all multiplication of two numbers
  *@argc:Number of argument
  *@argv:argument
- *Return: always 0
+ *Return: 0 for success, 1 for Error
  */
 
 int main(int argc, char *argv[])
@@ -18,12 +18,11 @@ if (argc != 3)
 	printf("Error\n");
 	return (1);
 }
-	else if (argc > 0)
-	{
+
 		mul1 = atoi(argv[1]);
 		mul2 = atoi(argv[2]);
 		n = mul1 * mul2;
 		printf("%i\n", n);
 return (0);
-	}
+
 }
