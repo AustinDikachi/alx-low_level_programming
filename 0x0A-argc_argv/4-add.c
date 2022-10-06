0a89f04f01f52d50cf9804aa8fd4dbe2b2531e99
austindikachi <augustineogazie@gmail.com>
2022-10-05 19:34:03 -0700

    View alx argc_argv C task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main - prints all argument it receives
+ *@argc:Number of argument
+ *@argv:argument
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+int i, j, sum = 0;
+
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (!isdigit(argv[i][j]))
+{
+printf("Error\n");
+return (1);
+}
+}
+sum = sum + atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
