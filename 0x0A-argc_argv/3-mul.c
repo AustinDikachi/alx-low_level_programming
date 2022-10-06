bcbe112d08e58cb83b6f1f238ac5e0026a896d75
austindikachi <augustineogazie@gmail.com>
2022-10-05 17:17:07 -0700

    View alx argc_argv C task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,33 +10,23 @@
 
 int main(int argc, char *argv[])
 {
-
-
-int i, mul1, mul2, mul;
+int i, mul1, mul2;
 int n = 2;
 
-printf("%d\n", argc);
-
-
-printf("Total = %d\n", mul);
 if (argc != 3)
+{
 	printf("Error\n");
-
-else if (argc > 0)
-    {
-        for(i = 0; i < argc; i++)
-        {
-            printf("argv[%d]=%s\n",i,  argv[i]);
-          }
-          
-            mul1 = atoi(argv[1]);
-            mul2 = atoi(argv[2]);
-            
-            n = mul1 * mul2;
-            
-            
-            printf("%d\n", n);
-
-        }
+}
+	else if (argc > 0)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("argv[%d] = %s\n", i, argv[i]);
+		}
+		mul1 = atoi(argv[1]);
+		mul2 = atoi(argv[2]);
+		n = mul1 * mul2;
+		printf("%d\n", n);
 return (1);
+	}
 }
